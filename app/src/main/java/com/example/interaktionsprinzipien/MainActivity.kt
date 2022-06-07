12bb5c9b9128321d3eeec2fbe03d8075c42febad
hanesio <han.richter@gmail.com>
2022-06-07 13:26:53 +0200

    Kommentar gelöscht

--- app/src/main/java/com/example/interaktionsprinzipien/MainActivity.kt
+++ app/src/main/java/com/example/interaktionsprinzipien/MainActivity.kt
@@ -3,9 +3,6 @@ package com.example.interaktionsprinzipien
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-/*
-Gittest Kommentar
- */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
