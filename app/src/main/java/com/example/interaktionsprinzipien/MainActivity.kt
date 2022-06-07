4b0a97efa6a754b9d9cf7f0344c837a55ca5be38
Jonas Feige <jonas.feige@study.hs-duesseldorf.de>
2022-06-07 13:02:11 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/interaktionsprinzipien/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.interaktionsprinzipien
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+Gittest Kommentar
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
