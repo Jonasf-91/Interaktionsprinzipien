9f1c0efb5fb3cbe27069e65655e52fdd0150acf5
Jonas Feige <jonas.feige@study.hs-duesseldorf.de>
2022-06-08 14:16:50 +0200

    die vier Hauptactivities angelegt
    buttons auf der Main Activity sind erst mal nur zum Testen und Ausprobieren der Activities. Kann sich jeder erst mal in seiner Activity Ausprobieren.

--- app/src/main/java/com/example/interaktionsprinzipien/MainActivity.kt
+++ app/src/main/java/com/example/interaktionsprinzipien/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.interaktionsprinzipien
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gameActivityBtn = findViewById<Button>(R.id.gameActivityBtn)
+        gameActivityBtn.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        val logInActivityBtn = findViewById<Button>(R.id.logInActivityBtn)
+        logInActivityBtn.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+
+        val optionActivityBtn = findViewById<Button>(R.id.optionActivityBtn)
+        optionActivityBtn.setOnClickListener {
+            val intent = Intent(this, OptionActivity::class.java)
+            startActivity(intent)
+        }
+        val editorActivityBtn = findViewById<Button>(R.id.editorActivityBtn)
+        editorActivityBtn.setOnClickListener {
+            val intent = Intent(this, EditorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
