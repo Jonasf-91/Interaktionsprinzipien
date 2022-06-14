4725ee8bc7df2a64ae9eab44c5131c81dc0d69d9
Jonas Feige <jonas.feige@study.hs-duesseldorf.de>
2022-06-14 17:40:08 +0200

    buttons implementiert

--- app/src/main/java/com/example/interaktionsprinzipien/GameActivity.kt
+++ app/src/main/java/com/example/interaktionsprinzipien/GameActivity.kt
@@ -1,11 +1,12 @@
 package com.example.interaktionsprinzipien
 
 
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_game.*
+import kotlinx.android.synthetic.main.activity_game_four_connect_content.*
 
 
 class GameActivity : AppCompatActivity(){
@@ -13,11 +14,19 @@ class GameActivity : AppCompatActivity(){
     private var countDownProgress : Int = 0
     private var countDownStart : Int = 6
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+        four_connect_btn_options.setOnClickListener {
+            val intent = Intent(this, OptionActivity::class.java)
+            startActivity(intent)
+        }
+
+        //starts OnClickListener for all arrowButtons
+        setUpArrowButtons()
+
+        //starts Countdown for the time above the playboard
         startCountdown()
 
     }
@@ -36,5 +45,46 @@ class GameActivity : AppCompatActivity(){
                 //TODO add sound highlight
             }
         }.start()
+    }
+
+    private fun setUpArrowButtons(){
+        ib_four_connect_arrow0.setOnClickListener {
+            resetArrows()
+            ib_four_connect_arrow0.setImageResource(R.drawable.four_connect_coin_test)
+        }
+        ib_four_connect_arrow1.setOnClickListener {
+            resetArrows()
+            ib_four_connect_arrow1.setImageResource(R.drawable.four_connect_coin_test)
+        }
+        ib_four_connect_arrow2.setOnClickListener {
+            resetArrows()
+            ib_four_connect_arrow2.setImageResource(R.drawable.four_connect_coin_test)
+        }
+        ib_four_connect_arrow3.setOnClickListener {
+            resetArrows()
+            ib_four_connect_arrow3.setImageResource(R.drawable.four_connect_coin_test)
+        }
+        ib_four_connect_arrow4.setOnClickListener {
+            resetArrows()
+            ib_four_connect_arrow4.setImageResource(R.drawable.four_connect_coin_test)
+        }
+        ib_four_connect_arrow5.setOnClickListener {
+            resetArrows()
+            ib_four_connect_arrow5.setImageResource(R.drawable.four_connect_coin_test)
+        }
+        ib_four_connect_arrow6.setOnClickListener {
+            resetArrows()
+            ib_four_connect_arrow6.setImageResource(R.drawable.four_connect_coin_test)
+        }
+    }
+
+    private fun resetArrows(){
+        ib_four_connect_arrow0.setImageResource(R.drawable.four_connect_arrow)
+        ib_four_connect_arrow1.setImageResource(R.drawable.four_connect_arrow)
+        ib_four_connect_arrow2.setImageResource(R.drawable.four_connect_arrow)
+        ib_four_connect_arrow3.setImageResource(R.drawable.four_connect_arrow)
+        ib_four_connect_arrow4.setImageResource(R.drawable.four_connect_arrow)
+        ib_four_connect_arrow5.setImageResource(R.drawable.four_connect_arrow)
+        ib_four_connect_arrow6.setImageResource(R.drawable.four_connect_arrow)
     }
 }
