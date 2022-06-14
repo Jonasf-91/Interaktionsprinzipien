0b3ab6371c167b9bd6ec87328b665b6aa7b277fc
hanesio <han.richter@gmail.com>
2022-06-14 17:18:43 +0200

    coin Creator rough

--- app/src/main/java/com/example/interaktionsprinzipien/EditorActivity.kt
+++ app/src/main/java/com/example/interaktionsprinzipien/EditorActivity.kt
@@ -1,11 +1,24 @@
 package com.example.interaktionsprinzipien
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class EditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
+
+        val btnColorMenu = findViewById<ImageButton>(R.id.btnColorMenu)
+        btnColorMenu.setOnClickListener {
+            val intent = Intent(this, EditorColorActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
+
 }
